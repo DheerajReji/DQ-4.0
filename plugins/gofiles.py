34f900481b82bb32252af469084c480ece1c8691
OB MOVIES OFFICIAL <56756946+owdver@users.noreply.github.com>
2021-08-17 00:00:51 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -147,8 +147,6 @@ async def query_mgs(client: Bot, message: Message):
                     chat_id=message.chat.id,
                     text=Presets.NO_MEDIA,
                     reply_to_message_id=user_message[id],
-                    parse_mode='html',
-                    disable_web_page_preview=True
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [InlineKeyboardButton(
