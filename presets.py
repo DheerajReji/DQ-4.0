f0db2b4b2f13d8b42140977b817d9c3bc6125c9c
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-09-29 18:17:26 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,8 +1,8 @@
 # ----------------------------------- https://github.com/m4mallu/gofilesbot ------------------------------------------ #
 
 class Presets(object):
-    CAPTION_TEXT_DOC = "<b>{}\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ @Mf_linKz</b>"
-    CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
+    CAPTION_TEXT_DOC = "<b>{}\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ @MF_linKz</b>"
+    CAPTION_TEXT_VID = "\n\n<mono>File Name:</mono> {}\n\n<b>Size:</b> {}"
     ASK_PM_TEXT = "<b>നിങ്ങൾ ചോദിച്ച മൂവി ലഭിക്കാൻ താഴെ കാണുന്ന ബട്ടണിൽ ക്ലിക്ക് ചെയ്യുക</b>"
     WELCOME_TEXT = "Hello.. <b>{}</b>\n<i>I can help you getting movies from</i> @MF_linKz "
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
