0c5853697eb528b0dd9b56a22898930a012bcc26
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-09-29 15:22:05 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,10 @@
 # ----------------------------------- https://github.com/m4mallu/gofilesbot ------------------------------------------ #
 
 class Presets(object):
-    CAPTION_TEXT_DOC = "<b>{}\n\n ♻️ 𝙅𝙊𝙄𝙉 :- https://t.me/joinchat/Ur8bdKHGNRdBoeHW </b>"
+    CAPTION_TEXT_DOC = "<b>{}\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ [@ᴍғʟɪɴᴋᴢ](https://t.me/MF_Linkz)</b>"
     CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
     ASK_PM_TEXT = "<b>നിങ്ങൾ ചോദിച്ച മൂവി ലഭിക്കാൻ താഴെ കാണുന്ന ബട്ടണിൽ ക്ലിക്ക് ചെയ്യുക</b>"
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<i>I can help you getting movies from</i> @OB_Movies "
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n<i>I can help you getting movies from</i> @MF_linKz "
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>For getting Movies from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
                   "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
